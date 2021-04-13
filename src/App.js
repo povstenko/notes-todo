602adb8f0dec649d86fb48c40cdc00a3a985dae2
Vitaliy Povstenko <vitaly.povstenko@gmail.com>
2021-04-13 14:02:18 +0300

    routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import AboutPage from './pages/AboutPage/AboutPage';
+import NotesPage from './pages/NotesPage/NotesPage';
+import TodoPage from './pages/TodoPage/TodoPage';
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      App
+      <Router>
+        <div id="appContainer">
+          <Switch>
+            <Route path="/" component={AboutPage} />
+            <Route path="/about" component={AboutPage} />
+            <Route path="/notes" component={NotesPage} />
+            <Route path="/todo" component={TodoPage} />
+            <Route component={NotFoundPage} />
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
