1cb9adff847ef95d274f3f451a4b23151c79a555
Vitaliy Povstenko <vitaly.povstenko@gmail.com>
2021-04-13 13:43:46 +0300

    generate react cli

---
+++ src/pages/AboutPage/AboutPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './AboutPage.css';
+
+const AboutPage = () => (
+  <div className="about-page">
+    <h1 className="about-header">About Page</h1>
+  </div>
+);
+
+AboutPage.propTypes = {};
+
+AboutPage.defaultProps = {};
+
+export default AboutPage;
