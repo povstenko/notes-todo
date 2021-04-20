65b955793726f029a566bd82fdeab09f00161b90
Vitaliy Povstenko <vitaly.povstenko@gmail.com>
2021-04-20 14:02:33 +0300

    add notes

--- src/components/NoteEditor/NoteEditor.js
+++ src/components/NoteEditor/NoteEditor.js
@@ -1,11 +1,46 @@
 import React from 'react';
 import './NoteEditor.css';
 
-const NoteEditor = () => (
-  <div className="NoteEditor">
-    NoteEditor Component
-  </div>
-);
+class NoteEditor extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {text: ''}
+    this.handleTextChange = this.handleTextChange.bind(this)
+    this.handleOnClick = this.handleOnClick.bind(this)
+  }
+
+  handleTextChange(e) {
+    this.setState({text: e.target.value})
+  }
+
+  handleOnClick() {
+    let newNote = {
+      id: Date.now(),
+      title: 'NoteTitle',
+      text: this.state.text,
+      color: '#feff9c'
+    }
+    this.props.onNoteAdd(newNote)
+  }
+
+  render() {
+    return <div className="note-editor">
+      <div className="row">
+        <div className="input-field col s10">
+          <i className="material-icons prefix">mode_edit</i>
+          <textarea onChange={this.handleTextChange} id="icon_prefix2" className="materialize-textarea"></textarea>
+          <label htmlFor="icon_prefix2">Add note</label>
+        </div>
+
+        <div className="input-field col s2">
+          <a onClick={this.handleOnClick} className="btn-floating btn-large waves-effect waves-light"><i className="material-icons">add</i></a>
+        </div>
+      </div>
+    </div>
+  }
+
+}
 
 NoteEditor.propTypes = {};
 
--- src/components/NotesGrid/NotesGrid.js
+++ src/components/NotesGrid/NotesGrid.js
@@ -5,15 +5,22 @@ import Masonry from 'masonry-layout'
 
 class NotesGrid extends React.Component {
   componentDidMount() {
-    var msnry = new Masonry('.notes-grid', {
+    this.msnry = new Masonry('.notes-grid', {
       itemSelector: '.note',
-      columnWidth: 400,
-      gutter: 10
+      columnWidth: 200,
+      gutter: 4
     });
+  }
+  componentDidUpdate(prevProps) {
+    if(this.props.notes.length !== prevProps.notes.length)
+    {
+      this.msnry.reloadItems()
+      this.msnry.layout()
+    }
   }
 
   render() {
-    return <div className="notes-grid">
+    return <div className="notes-grid row">
       {
         this.props.notes.map((note) => {
           return <Note
